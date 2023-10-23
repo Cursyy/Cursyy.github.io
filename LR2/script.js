5a5ba4400bf47bcc84edc53cc4dba569ac955254
Cursyy <112431108+Cursyy@users.noreply.github.com>
2023-10-23 11:35:29 +0100

    LR2

--- LR2/script.js
+++ LR2/script.js
@@ -1,13 +1,13 @@
 const btn = document.getElementById('change_btn');
-let titles = document.getElementsByTagName('h2');
-let paragraphs = document.getElementsByTagName('p');
+let titles = document.querySelectorAll('h2');
+let paragraphs = document.querySelectorAll('p');
 let cells = document.getElementsByTagName('td');
 
 btn.addEventListener('click', () => {
-    for(let i = 1; i < 8; i+=3){
+    for(let i = 1, j = 0; i < 8, j<=2; i+=3, j++){
         cells[i].innerHTML = '';
-        cells[i].appendChild(document.getElementById(i));
+        cells[i].appendChild(paragraphs[j]);
         cells[i-1].innerHTML = '';
-        cells[i-1].appendChild(document.getElementById(i-1));
+        cells[i-1].appendChild(titles[j]);
     }
 })
