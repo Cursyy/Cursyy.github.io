df5a4ee8249bbfe4e55270809e2922bb57a377a3
Cursyy <112431108+Cursyy@users.noreply.github.com>
2023-10-30 09:43:07 +0000

    portfolio (PR_2 added)

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -1,19 +1,19 @@
-const iconWrap = document.getElementById('iconWrap');
-const iconOpen = document.getElementById('iconOpen');
-const iconClose = document.getElementById('iconClose');
-const mainMenu = document.getElementById('mainMenu');
+const iconWrap = document.getElementById("iconWrap");
+const iconOpen = document.getElementById("iconOpen");
+const iconClose = document.getElementById("iconClose");
+const mainMenu = document.getElementById("mainMenu");
 
-iconWrap.addEventListener('click',() => {
-	mainMenu.classList.toggle('hide-menu');
-	iconOpen.classList.toggle('hide');
-	iconClose.classList.toggle('hide');
-})
+iconWrap.addEventListener("click", () => {
+  mainMenu.classList.toggle("hide-menu");
+  iconOpen.classList.toggle("hide");
+  iconClose.classList.toggle("hide");
+});
 
-function myFunction() {
-	var x = document.getElementById("myLinks");
-	if (x.style.display === "block") {
-	  x.style.display = "none";
-	} else {
-	  x.style.display = "block";
-	}
-}
+let list_boxes = document.querySelectorAll(".list_box");
+let list_btns = document.querySelectorAll(".list_btn");
+list_btns.forEach((element) => {
+  element.addEventListener("click", () => {
+    let btn_id = element.id.slice(-1);
+    list_boxes[btn_id - 1].classList.toggle("open");
+  });
+});
